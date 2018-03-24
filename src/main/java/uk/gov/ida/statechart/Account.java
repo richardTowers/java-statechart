ac6c888c0ac5ea3e8dda1007e9564c6122ce252f
Richard Towers <richard_towers@yahoo.co.uk>
2018-03-24 15:48:12 +0000

    Implement something similar to raganwalds transitionsTo style machine
    This is nice in the sense that we can clearly see what the transitions are, and in which states
    they're valid. It's a bit meh because we sacrifice a type safety (in the sense that all the
    possible transitions are available on the base state, so there's no way for the compiler to
    know whether a transition is legal - it will just blow up at runtime).

--- src/main/java/uk/gov/ida/statechart/Account.java
+++ src/main/java/uk/gov/ida/statechart/Account.java
@@ -2,74 +2,62 @@ package uk.gov.ida.statechart;
 
 import java.math.BigDecimal;
 
-public class Account {
-  private String state = "open";
-  private BigDecimal balance = new BigDecimal("0");
+public final class Account {
 
-  public void deposit(BigDecimal amount) {
-    if (this.state == "open" || this.state == "held") {
-      this.balance = this.balance.add(amount);
-    }
-    else {
-      throw new RuntimeException("Invalid event");
-    }
+  private abstract class AccountState {
+    public final String name;
+    public final BigDecimal balance;
+
+    AccountState(String name, BigDecimal balance) { this.name = name; this.balance = balance; }
+
+    AccountState deposit(BigDecimal amount) { throw new RuntimeException("Invalid event"); }
+    AccountState withdraw(BigDecimal amount) { throw new RuntimeException("Invalid event"); }
+    ClosedAccountState close() { throw new RuntimeException("Invalid event"); }
+    HeldAccountState placeHold() { throw new RuntimeException("Invalid event"); }
+    OpenAccountState removeHold() { throw new RuntimeException("Invalid event"); }
+    OpenAccountState reopen() { throw new RuntimeException("Invalid event"); }
   }
 
-  public void withdraw(BigDecimal amount) {
-    if (this.state == "open") {
-      this.balance = this.balance.subtract(amount);
-    }
-    else {
-      throw new RuntimeException("Invalid event");
-    }
+  private final class OpenAccountState extends AccountState {
+    OpenAccountState() { this(BigDecimal.ZERO); }
+    OpenAccountState(BigDecimal balance) { super("open", balance); }
+
+    @Override
+    public OpenAccountState deposit(BigDecimal amount) { return new OpenAccountState(this.balance.add(amount)); }
+    @Override
+    public OpenAccountState withdraw(BigDecimal amount) { return new OpenAccountState(this.balance.subtract(amount)); }
+    @Override
+    public HeldAccountState placeHold() { return new HeldAccountState(this.balance); }
+    @Override
+    public ClosedAccountState close() { return new ClosedAccountState(this.balance); }
   }
 
-  public void placeHold() {
-    if (this.state == "open") {
-      this.state = "held";
-    }
-    else {
-      throw new RuntimeException("Invalid event");
-    }
+  private final class HeldAccountState extends AccountState {
+    HeldAccountState(BigDecimal balance) { super("held", balance); }
+
+    @Override
+    public OpenAccountState removeHold() { return new OpenAccountState(this.balance); }
+    @Override
+    public OpenAccountState deposit(BigDecimal amount) { return new OpenAccountState(this.balance.add(amount)); }
+    @Override
+    public ClosedAccountState close() { return new ClosedAccountState(this.balance); }
   }
 
-  public void removeHold() {
-    if (this.state == "held") {
-      this.state = "open";
-    }
-    else {
-      throw new RuntimeException("Invalid event");
-    }
+  private final class ClosedAccountState extends AccountState {
+    ClosedAccountState(BigDecimal balance) { super("closed", balance); }
+    @Override
+    public OpenAccountState reopen() { return new OpenAccountState(this.balance); }
   }
 
-  public void close() {
-    if (this.state == "open") {
-      if (this.balance.compareTo(BigDecimal.ZERO) > 0) {
-        // ...transfer balance into suspension account
-      }
-      this.state = "closed";
-    }
-    else {
-      throw new RuntimeException("Invalid event");
-    }
-  }
+  private AccountState state = new OpenAccountState();
 
-  public void reopen() {
-    if (this.state == "closed") {
-      // ...restore balance if applicable
-      this.state = "open";
-    }
-    else {
-      throw new RuntimeException("Invalid event");
-    }
-  }
+  public void deposit(BigDecimal amount) { state = state.deposit(amount); }
+  public void withdraw(BigDecimal amount) { state = state.withdraw(amount); }
+  public void placeHold() { state = state.placeHold(); }
+  public void removeHold() { state = state.removeHold(); }
+  public void close() { state = state.close(); }
+  public void reopen() { state = state.reopen(); }
 
-  public String getState() {
-    return this.state;
-  }
-
-  public BigDecimal getBalance() {
-    return this.balance;
-  }
-  
+  public String getState() { return this.state.name; }
+  public BigDecimal getBalance() { return this.state.balance; }
 }
