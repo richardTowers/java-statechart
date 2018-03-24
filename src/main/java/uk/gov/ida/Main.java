acbe202fc3b4f56ec34022df229c99dfb432d2ef
Richard Towers <richard_towers@yahoo.co.uk>
2018-03-24 21:46:56 +0000

    Switch from List to Set to avoid duplicates

--- src/main/java/uk/gov/ida/Main.java
+++ src/main/java/uk/gov/ida/Main.java
@@ -1,8 +1,8 @@
 package uk.gov.ida;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import uk.gov.ida.reflection.StatechartAnalyzer;
 import uk.gov.ida.statechart.Account;
@@ -10,16 +10,16 @@ import uk.gov.ida.statechart.AccountState;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Map<String, List<String>>> transitions = StatechartAnalyzer.getTransitions(AccountState.class);
+        Map<String, Map<String, Set<String>>> transitions = StatechartAnalyzer.getTransitions(AccountState.class);
         System.out.println("@startuml");
         System.out.println("skinparam monochrome true");
         System.out.println("skinparam Shadowing false");
 
         String initialState = new Account().getState();
         System.out.println("[*] --> " + initialState);
-        for (Entry<String, Map<String, List<String>>> currentStateEntry : transitions.entrySet()) {
+        for (Entry<String, Map<String, Set<String>>> currentStateEntry : transitions.entrySet()) {
             String currentStateName = currentStateEntry.getKey();
-            for (Entry<String, List<String>> nextStateEntry : currentStateEntry.getValue().entrySet()) {
+            for (Entry<String, Set<String>> nextStateEntry : currentStateEntry.getValue().entrySet()) {
                 String nextStateName = nextStateEntry.getKey();
                 System.out.println(currentStateName + " --> " + nextStateName + ": " + String.join(", ", nextStateEntry.getValue()));
             }
--- src/main/java/uk/gov/ida/reflection/StatechartAnalyzer.java
+++ src/main/java/uk/gov/ida/reflection/StatechartAnalyzer.java
@@ -2,13 +2,13 @@ package uk.gov.ida.reflection;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.mapping;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import uk.gov.ida.statechart.annotations.State;
 import uk.gov.ida.statechart.annotations.Transition;
@@ -23,10 +23,10 @@ public class StatechartAnalyzer {
    * Takes an outer class, searches for all inner classes marked with State annotations,
    * and then establishes the transitions permitted by those States.
    * 
-   * Returns a Map of state to next state to a list of all the named
+   * Returns a Map of state to next state to a set of all the named
    * transitions. This is useful for drawing diagrams.
    */
-  public static <T> Map<String, Map<String, List<String>>> getTransitions(Class<T> containerClass) {
+  public static <T> Map<String, Map<String, Set<String>>> getTransitions(Class<T> containerClass) {
     return Arrays.stream(containerClass.getDeclaredClasses())
       .filter(declaredClass -> declaredClass.isAnnotationPresent(State.class))
       .collect(toMap(
@@ -38,13 +38,13 @@ public class StatechartAnalyzer {
   /**
    * Takes a class with a State annotation and works out all the transitions that state can perform.
    * 
-   * Returns a map of next state to list of transitions that reach that state.
+   * Returns a map of next state to set of transitions that reach that state.
    */
-  private static Map<String, List<String>> getNextStateTransitions(Class<?> currentStateClass) {
+  private static Map<String, Set<String>> getNextStateTransitions(Class<?> currentStateClass) {
     return Arrays.stream(currentStateClass.getDeclaredMethods())
       .filter(method -> method.isAnnotationPresent(Transition.class))
       .map(transitionMethod -> new StringPair(transitionMethod.getName(), getNewStateName(currentStateClass, transitionMethod)))
-      .collect(groupingBy(pair -> pair.right, mapping(pair -> pair.left, toList())));
+      .collect(groupingBy(pair -> pair.right, mapping(pair -> pair.left, toSet())));
   }
 
   /**
